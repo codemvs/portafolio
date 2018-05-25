e5e3c75a8b8f460b1976529080846d2716d57533
Git MCS <mv.salvador93@gmail.com>
2018-05-24 22:17:59 -0500

    Add portafolio-item

---
+++ src/app/components/portafolio-item/portafolio-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portafolio-item',
+  templateUrl: './portafolio-item.component.html',
+  styles: []
+})
+export class PortafolioItemComponent{
+
+}
