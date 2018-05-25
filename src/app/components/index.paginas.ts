4a006c4b9cbdfd67bc259a67bf5ba2be3329b984
Git MCS <mv.salvador93@gmail.com>
2018-05-24 22:46:12 -0500

    Rutas agregadas

---
+++ src/app/components/index.paginas.ts
@@ -0,0 +1,6 @@
+
+// declaracion de rutas
+
+export {AboutComponent} from "./about/about.component";
+export {PortafolioComponent} from "./portafolio/portafolio.component";
+export {PortafolioItemComponent} from "./portafolio-item/portafolio-item.component";
